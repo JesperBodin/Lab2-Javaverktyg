ea86c005f571be7c344405e3153a7b4978675862
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-15 18:35:48 +0100

    Rename EmployeeManager.java to Employees.java

--- src/main/java/com/example/Employees.java
+++ src/main/java/com/example/Employees.java
@@ -2,12 +2,12 @@ package com.example;
 
 import java.util.List;
 
-public class EmployeeManager {
+public class Employees {
 
 	private final EmployeeRepository employeeRepository;
 	private final BankService bankService;
 
-	public EmployeeManager(EmployeeRepository employeeRepository, BankService bankService) {
+	public Employees(EmployeeRepository employeeRepository, BankService bankService) {
 		this.employeeRepository = employeeRepository;
 		this.bankService = bankService;
 	}
